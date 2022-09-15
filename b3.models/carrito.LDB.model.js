30b328a39173ec0d6c46dbff2aa945a9994c8fea
gdmatosc <g.dante.matos.c@outlook.com>
2022-09-15 13:47:26 -0500

    Se agregan configuraciones de mongoDB en la nube

--- b3.models/carrito.LDB.model.js
+++ b3.models/carrito.LDB.model.js
@@ -1,10 +1,31 @@
 const mongoose=require('mongoose');
-const carritoCollection='carritoProductos'
 
-let connCarritoLDB=mongoose.createConnection('mongodb://localhost:27017/dbCoderTest',{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
-})
+const MONGO_OPTIONS={
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  maxPoolSize: 50,
+  autoIndex: false,
+  retryWrites: false
+}
+
+const MONGO_USERNAME=process.env.MONGO_USERNAME;
+const MONGO_PASSWORD=process.env.MONGO_PASSWORD;
+const MONGO_HOST=process.env.MONGO_URL;
+
+const MONGO={
+  host: MONGO_HOST,
+  username: MONGO_USERNAME,
+  password: MONGO_PASSWORD,
+  options: MONGO_OPTIONS,
+  urlC: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}`,
+  urlL: 'mongodb://localhost:27017/dbCoderTest'
+}
+
+let connCarritoLDB=mongoose.createConnection(MONGO.urlC,MONGO_OPTIONS)
+
+const carritoCollection='carritoProductos'
 
 const productosSchema=new mongoose.Schema({
     nombre:{type:String, require:true,max:100},
